3ab429bd21bda92d7e58c08cee47d6a118b19635
Isabel Solano <misolanob@gmail.com>
2022-05-05 22:27:39 -0600

    Nuevo fetch y cambios en el html
    Nuevo fetch, no hace display todavía pero por el momento nada truena, por lo que guardo antes de que pase algo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import { useState } from "react"
+import { useEffect } from "react"
 
+//Laberinto
+const GetMaze = async (ancho, alto) => {
+    const JEC = 'https://maze.juanelcaballo.club/?type=json&w=#&h=%'
+    const JEC_temp = JEC.replace('#',ancho)
+    const JEC_mod = JEC_temp.replace('%', alto)
+    //fetch
+    const laberinto = await fetch(JEC_mod)
+        .then((res) => res.json())
+        .then((resJSON) => resJSON)
+    return laberinto
+}
 
+//Aplicación
 const App = () => {
     const [inicio, setInicio] = useState(true)
+    const [altura, setAltura] = useState(4)
+    const [ancho, setAncho] = useState(4)
 
     const handleClick = () => {
         setInicio(false)
-        console.log(inicio)
+    }
+
+    //get valores
+    const getAltura = (cant) => {
+        const alt = Number(cant.target.value)
+        setAltura(alt)
+    }
+
+    const getAncho = (cant) => {
+        const anch = Number(anch.target.value)
+        setAncho(anch)
     }
 
     const Cuadro = () => {
@@ -16,8 +41,8 @@ const App = () => {
                     <h1>Laberinto</h1>
                 </div>
                 <div>
-                    <input type="number" placeholder="altura" min={1}/>
-                    <input type="number" placeholder="ancho" min={1}/>
+                    <input id="altura" type="number" min={3} max={15} defaultValue={4} onChange={getAltura}/>
+                    <input id="ancho" type="number" min={3} max={15} defaultValue={4} onChange={getAncho}/>
                     <button onClick={handleClick}>
                         Comenzar juego
                     </button>
@@ -29,18 +54,14 @@ const App = () => {
     
 
     const Laberinto = () => {
-        fetch('https://maze.juanelcaballo.club/?type=json&w=4&h=4')
-        .then((response) => {
-            return response.json()
-        })
-        .then((res) => {console.log(res)})
-        
+
 
         return(
             <div>
                 <h1>Imaginen que aparece un laberinto xd</h1>
                 <div className="contenedor-laberinto">
-                    <></>
+                    <p>altura = {altura} y ancho = {ancho}</p>
+                    
                 </div>
             </div>
         )
